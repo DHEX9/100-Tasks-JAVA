c868ff3e8baecce21fbf81c5ec7479bbbc7de11d
dhex9 <danielrodrigueslg6@gmail.com>
2026-05-17 14:30:00 -0300

    Task_2

---
+++ Task_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite um número: ");
+        int num = scanner.nextInt();
+        
+        if(num%2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        scanner.close();
+    }   
+}
