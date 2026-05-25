6b3cff782092eaf41f95496e873070e293224865
dhex9 <danielrodrigueslg6@gmail.com>
2026-05-24 22:33:59 -0300

    Task 6

---
+++ Task_6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task_6 {
+        static Scanner scanner = new Scanner(System.in);
+    
+	public static void main(String[] args) {
+	    
+	    int num, total = 1;
+	    
+		System.out.println("Digite um número:");
+		num = scanner.nextInt();
+		for(int i = num; i > 0; i--){
+		    total *=  i;
+		}
+		System.out.println("Fatorial de " + num + ": " + total);
+	}
+}
