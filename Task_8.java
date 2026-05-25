cb5dc6c599e47e038b06e7ddc7c16fe9e934b2c3
dhex9 <danielrodrigueslg6@gmail.com>
2026-05-25 20:12:08 -0300

    Task 8

--- Task_8.java
+++ Task_8.java
@@ -4,8 +4,9 @@ public class Task_8 {
 
     static Scanner scanner = new Scanner(System.in);
 
-    // Solução 1
     public static void main(String[] args){
+
+        // Solução 1
 
         System.out.println("Digite uma palavra:");
 
@@ -19,8 +20,22 @@ public class Task_8 {
             System.out.println("Não Palíndromo");
         }
         
-    // Solução 2
+        // Solução 2
 
+        System.out.println("Digite uma palavra:");
+
+        String palavra2 = scanner.next();
+        StringBuilder invertido2 = new StringBuilder("");
+
+        for(int i = palavra2.length() - 1; i >= 0; i--){
+            invertido2.append(palavra2.charAt(i));
+        }
         
+        if(palavra2.equalsIgnoreCase(invertido2.toString())){
+            System.out.println("Palíndromo");
+        }
+        else{
+            System.out.println("Não Palíndromo");
+        }
     }
 }
