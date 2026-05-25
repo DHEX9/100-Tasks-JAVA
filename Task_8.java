b912f512a5353069c27a90c1573f7af1609b723c
dhex9 <danielrodrigueslg6@gmail.com>
2026-05-25 20:17:09 -0300

    Task 8

--- Task_8.java
+++ Task_8.java
@@ -19,13 +19,13 @@ public class Task_8 {
         else{
             System.out.println("Não Palíndromo");
         }
-        
+
         // Solução 2
 
         System.out.println("Digite uma palavra:");
 
         String palavra2 = scanner.next();
-        StringBuilder invertido2 = new StringBuilder("");
+        StringBuilder invertido2 = new StringBuilder();
 
         for(int i = palavra2.length() - 1; i >= 0; i--){
             invertido2.append(palavra2.charAt(i));
