13c9f1576e65c4b278d3be12467840ce4a50e370
dhex9 <danielrodrigueslg6@gmail.com>
2026-05-25 13:37:36 -0300

    Task 3

--- Task_4.java
+++ Task_4.java
@@ -4,24 +4,18 @@ public class Task_4 {
     static Scanner scanner = new Scanner(System.in);
     
 	public static void main(String[] args) {
-        
+
+		int num, unidade, invertido = 0;
+
 	    System.out.println("Digite uma sequencia de números:");
-	    String numbers = scanner.nextLine();
+	    num = scanner.nextInt();
 	    
-	    // Solução 1
-	    
-	    /*
-	    String invertida = new StringBuilder(numbers).reverse().toString();
-	    System.out.println(invertida);
-	    */
-	    
-	    // Solução 2
-	    
-	    String invertida = "";
-	    for(int i = numbers.length() -1; i >= 0; i --){
-	        invertida += numbers.charAt(i);
-	    }
-	    
-	    System.out.println(invertida);
+		while(num != 0){
+			unidade = num % 10;
+			num = num / 10;
+			invertido = invertido * 10 + unidade;
+		}
+		
+	    System.out.println(invertido);
 	}
 }
