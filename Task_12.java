ba3115cc05bd48b597c94ca4a877bc3732441e5e
dhex9 <danielrodrigueslg6@gmail.com>
2026-05-28 20:23:35 -0300

    Task 12

---
+++ Task_12.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Task_12 {
+    static Scanner scanner = new Scanner(System.in);
+
+    
+    public static void main(String[] args){
+        int num, num2;
+        int div = 2, mmc = 1, mdc = 1;
+        System.out.println("Entre com dois valores:");
+        num = scanner.nextInt(); num2 = scanner.nextInt();
+
+        while((num != 1) || (num2 != 1)){
+            
+            if(num%div == 0 || num2%div == 0){
+                System.out.println(div + " | " + num + " " + num2);
+
+                mmc *= div;
+
+                if(num%div == 0 && num2%div == 0){
+                    mdc *= div;
+                }
+                
+                if(num%div == 0){
+                    num /= div;
+                }
+   
+                if(num2%div == 0){
+                    num2 /= div;
+                }
+
+                    
+           }
+           else{
+                div++;
+            }
+        }
+        System.out.println("  | " + num + " " + num2);
+        System.out.println("MDC: "+ mdc + ", MMC: " + mmc);
+    }
+}
