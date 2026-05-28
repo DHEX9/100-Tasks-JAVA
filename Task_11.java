0611568d114ec522747294c99ad7034138fcd41c
dhex9 <danielrodrigueslg6@gmail.com>
2026-05-28 18:27:05 -0300

    Task 11

---
+++ Task_11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task_11 {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Entre com seu número:");
+        int num = scanner.nextInt();
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println(num + "x" + i + " = " + num*i);
+        }
+    }
+}
