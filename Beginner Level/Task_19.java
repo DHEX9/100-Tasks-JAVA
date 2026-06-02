df1060ad16aad53677b967112c1e47a1c4f3b56d
dhex9 <danielrodrigueslg6@gmail.com>
2026-06-02 17:36:31 -0300

    Task_19

---
+++ Beginner Level/Task_19.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task_19 {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Entre com seu número:");
+
+        int num = scanner.nextInt(), counter = 0;
+
+        //Com string
+        String str_num = String.valueOf(num);
+        System.out.println("Com string: " + str_num.length());
+
+        //Com int
+        while(num != 0){
+            num /= 10;
+            counter++;
+        }
+
+        System.out.println("Com int: " + counter);
+    }
+}
