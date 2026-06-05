57d5a6862621a7006d7f8e68bd7ebd0123e93211
dhex9 <danielrodrigueslg6@gmail.com>
2026-06-05 18:35:25 -0300

    Task_28

--- Intermediate_Level/Task_28.java
+++ Intermediate_Level/Task_28.java
@@ -1,3 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task_28 {
-    
+    public static void main(String[] args){
+        System.out.print("Entre com duas palavras: ");
+
+        Scanner scanner = new Scanner(System.in);
+        char[] str1 = scanner.next().toLowerCase().toCharArray();
+        char[] str2 = scanner.next().toLowerCase().toCharArray();
+        scanner.close();
+
+        if(str1.length != str2.length){
+            System.out.println("Não é Anagrama");
+            return;
+        }
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        for(int i = 0; i < str1.length; i++){
+            if(str1[i] != str2[i]){
+                System.out.println("Não é Anagrama");
+                return;
+            }
+        }
+        System.out.println("É Anagrama");
+    }
 }
