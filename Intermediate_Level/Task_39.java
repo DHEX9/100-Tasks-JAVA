57456fe7378199198764541a50bbedf7b577d0c2
dhex9 <danielrodrigueslg6@gmail.com>
2026-06-09 13:09:00 -0300

    Task_39

--- Intermediate_Level/Task_39.java
+++ Intermediate_Level/Task_39.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Task_39 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entre com seu exemplo: ");
+        String characters = scanner.nextLine();
+        scanner.close();
+
+        for (int i = 0; i < characters.length(); i++) {
+            System.out.printf("%c: %d ", characters.charAt(i), (int)characters.charAt(i));
+        }
+    }
 }
